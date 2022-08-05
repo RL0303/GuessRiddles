cfb414a75fdaedb8f0f3d59e84201c96080e33d9
Robert <linrobert0303@gmail.com>
2022-08-05 15:33:50 +0800

    Initial Commit

---
+++ GuessRiddles/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuessRiddles
+//
+//  Created by 林辰澤 on 2022/8/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
